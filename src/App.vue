428212ac159d65e5c4e02c11ba0f2b5a7494e31a
Faith <wanjirufaith83@gmail.com>
2019-06-20 09:41:04 +0300

    dynamic-form

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,98 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    
+   
+
+    <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/>
+    <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
+    <FormAddress v-if="currentStepNumber === 3" @update="processStep"/>
+    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep" :wizard-data="form "/>
+
+    <div class="progress-bar">
+      <div :style="`width: ${progress}%;`"></div>
+    </div>
+
+    <!-- Actions -->
+    <div class="buttons">
+      <button
+        @click="goBack" 
+        v-if="currentStepNumber > 1"
+        class="btn-outlined">
+        Back</button>
+
+      <button
+        @click="goNext"
+        :disabled="!canGoNext"
+        class="btn">
+        Next</button>
+    </div>
+
+    <pre><code>{{form}}</code></pre>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+// import FormWizard from './components/FormWizard.vue'
+// export default {
+  // name: 'app',
+  // components: {
+  //   FormWizard
+  // }
+// }
+import FormPlanPicker from './components/FormPlanPicker'
+import FormUserDetails from './components/FormUserDetails'
+import FormAddress from './components/FormAddress'
+import FormReviewOrder from './components/FormReviewOrder'
+import FormWizard from './components/FormWizard.vue'
+import app from '../public/app.css'
+export default {
+  name: 'app',
+  components: {
+    FormPlanPicker,
+    FormUserDetails,
+    FormAddress,
+    FormReviewOrder
+  },
+  data () {
+    return {
+      currentStepNumber: 1,
+      canGoNext:false,
+      length: 4,
+      form: {
+        plan: null,
+        email: null,
+        name: null,
+        password: null,
+        address: null,
+        recipient: null,
+        chocolate: false,
+        otherTreat: false
+      }
+    }
+  },
+  computed: {
+    progress () {
+      return this.currentStepNumber/this.length * 100
+    }
+  },
+  methods: {
+    processStep(stepData){
+      Object.assign(this.form, stepData)
+      this.canGoNext = true
+    },
+    goBack () {
+      this.currentStepNumber--
+    },
+    goNext () {
+      this.currentStepNumber++
+      this.canGoNext = false
     }
   }
 }
+</script> 
+
+<style lang="scss">
+
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+//import wizard from './components.FormWizard.vue'
+import FormPlanPicker from './components/FormPlanPicker'
 
 Vue.use(Router)
 
@@ -8,18 +10,19 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: Home
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
+    {path: '/', name:'FormPlanPicker', component:FormPlanPicker}
   ]
 })
